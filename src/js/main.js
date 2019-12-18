6b3b150d4ed6e99be9072760a16d9b8ed7c2abd2
hoangviethung <hoangviet.hung.009@gmail.com>
2019-12-18 11:58:54 +0700

    giao diện ( đã có mobile) -> có kèm video 30s

--- src/js/main.js
+++ src/js/main.js
@@ -89,12 +89,218 @@ function SVG() {
 	});
 }
 
+const clickGoTop = () => {
+	let goTopButton = document.getElementById('button-to-top');
+	if (goTopButton) {
+		goTopButton.addEventListener('click', () => {
+			window.scrollTo({
+				top: 0,
+				behavior: 'smooth',
+			})
+		})
+	}
+}
+
+function sliderBigIMG() {
+	var slideBigIMG = new Swiper('.index-3 .swiper-container', {
+		speed: 1000,
+		autoplay: {
+			delay: 10000,
+		},
+		// Navigation arrows
+		navigation: {
+			nextEl: '.index-3 .swiper-button-next',
+			prevEl: '.index-3 .swiper-button-prev',
+		},
+		on: {
+			slideChangeTransitionEnd: function() {
+				// VDIEO CHẠY
+				const video = document.querySelector('.index-3 .swiper-container .swiper-slide-active video');
+				// VIDEO DỪNG
+				const pause_video = document.querySelector('.index-3 .swiper-container .swiper-slide video');
+				if (video) {
+					video.play();
+					console.log('play');
+				} else {
+					pause_video.pause();
+					console.log('pause');
+				}
+			}
+		}
+	});
+}
+
+function fancyboxOninit() {
+	const video = document.querySelector('.index-3 .swiper-container video');
+
+	$('#fancyboxOninit').fancybox({
+		afterClose: function() {
+			video.play()
+		}
+	})
+
+	$('#fancyboxOninit').trigger('click')
+}
+
+function sliderChooseCard() {
+	var sliderChooseCard = new Swiper('.slider-choose-card .swiper-container', {
+		simulateTouch: false,
+		slidesPerView: 5,
+		spaceBetween: 8,
+		// Navigation arrows
+		navigation: {
+			nextEl: '.slider-choose-card .swiper-button-next',
+			prevEl: '.slider-choose-card .swiper-button-prev',
+		},
+	});
+}
+
+function scrollMenu() {
+	$('.list-nav .nav-item .link').on('click', function() {
+		$('body').removeClass('disabled');
+		$('.overlay').removeClass('active');
+		$('.list-nav').removeClass('active');
+		$('.button-mobile').removeClass('active');
+		let url = $(this).attr('data-href');
+		$('html, body').animate({
+			scrollTop: $(url).offset().top - 123 // Means Less header height
+		}, 1000);
+	});
+}
+
+function chooseIMG() {
+	// CHỌN HÌNH MẶC ĐỊNH TỪ ĐẦU
+	var imgUrl = $('.choose-image .list-item .item').find('img').attr('src');
+	$('.result-choose-img figure img').attr('src', imgUrl);
+
+	$('.choose-image .list-item .item').on('click', function() {
+		// CHỌN HÌNH
+		$(this).addClass('checked');
+		$('.choose-image .list-item .item').not(this).removeClass('checked');
+		// ĐỔI HÌNH Ở BLOCK KẾT QUẢ
+		imgUrl = $(this).find('img').attr('src');
+		$('.result-choose-img figure img').attr('src', imgUrl);
+	})
+}
+
+function chooseCard() {
+	$('.slider-choose-card figure').on('click', function() {
+		$(this).addClass('checked');
+		$('.slider-choose-card .swiper-slide figure').not(this).removeClass('checked');
+	})
+}
+
+function chooseContentDefault() {
+	// CHỌN TEXT MẶC ĐỊNH TỪ ĐẦU
+	var textDefault = $('.choose-to-write .item.checked').text();
+	$('.write textarea').html(textDefault);
+
+	$('.choose-to-write .item').on('click', function() {
+		// RESET KHI CHỌN TEXT
+		$('.write textarea').val('');
+		// CHECKED
+		$(this).addClass('checked');
+		$('.choose-to-write .item').not(this).removeClass('checked');
+		// NỘI DUNG TEXT MẪU
+		textDefault = $(this).text();
+		// SET NỘI DUNG VÀO BOX WRITE
+		$('.write textarea').val(textDefault);
+	});
+}
+
+function chooseBackground() {
+	$('.write .list-bottom .background span').on('click', function() {
+		$(this).addClass('checked');
+		$('.write .list-bottom .background span').not(this).removeClass('checked');
+	})
+}
+
+// SHOW MENU IN MOBILE
+function showMenuMobile() {
+	$('.button-mobile').on('click', function() {
+		$(this).toggleClass('active');
+		$(this).siblings('.list-nav').toggleClass('active');
+		$('body').toggleClass('disabled');
+		$('.overlay').toggleClass('active');
+	});
+
+	$('.overlay').on('click', function() {
+		$(this).removeClass('active');
+		$('.list-nav').removeClass('active');
+		$('body').removeClass('disabled');
+		$('.button-mobile').toggleClass('active');
+	});
+}
+
+function showContent() {
+	var result = $('.write textarea').val();
+	$('.result figure figcaption p').html(result);
+
+	$('.write textarea').on('keyup', function() {
+		result = $(this).val();
+		console.log(result);
+		$('.result figure figcaption p').html(result);
+	});
+}
+
+function ajaxForm() {
+	$('.index-4 button').on('click', function() {
+		// CÁC TRƯỜNG INPUT
+		const name = $('.index-4 #name').val();
+		const email = $('.index-4 #email').val();
+		const phone = $('.index-4 #phone').val();
+		const content = $('.index-4 #content').val();
+		// URL GỬI DATA
+		const url = $(this).attr('data-url');
+		// AJAX GỬI DATA
+		$.ajax({
+			type: "POST",
+			url: url,
+			data: {
+				name: name,
+				emai: email,
+				phone: phone,
+				content: content
+			},
+			dataType: "JSON",
+			error: function(err) {
+				alert('Cảm ơn bạn đã đăng kí');
+			},
+			success: function(res) {
+				alert(res.Message)
+			}
+		});
+	});
+}
+
 // CHẠY KHI DOCUMENT SẴN SÀNG
 document.addEventListener('DOMContentLoaded', () => {
 	// LOADING
 	loading();
 	// SVG CONTROL
 	SVG();
+	// FANCYBOX
+	// fancyboxOninit();
+	// MENU
+	scrollMenu();
+	clickGoTop();
+	// CHOOSE IMAGE
+	chooseIMG();
+	// CHOOSE CARD
+	chooseCard();
+	// CHỌN CÂU CHÚC MẶC ĐỊNH
+	chooseContentDefault();
+	// CHỌN MÀU CHO BACKGROUND
+	chooseBackground();
+	// SHOW CONTENT
+	showContent();
+	// AJAX FORM
+	ajaxForm();
+	// MENU MOBILE
+	showMenuMobile();
+	// SLIDER
+	sliderBigIMG();
+	sliderChooseCard();
 });
 
 // CHẠY KHI WINDOWN SCROLL
